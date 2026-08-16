0c8d7ff18e6a6963cafc7974af406bbb09fe4eeb
ahmatsamah01-cell <ahmatsamah01@gmail.com>
2026-08-16 15:49:54 +0100

    Update AppLayout.tsx

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -182,7 +182,7 @@ export function AppLayout({ children }: { children: ReactNode }) {
               </p>
             </div>
           </Link>
-
+          </header>
         <main className="min-w-0 flex-1 p-4 sm:p-6 lg:p-8">
           {children}
         </main>
