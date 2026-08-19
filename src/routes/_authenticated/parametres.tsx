cbc56392c82eb44946f8ecff59af842150866331
ahmatsamah01-cell <ahmatsamah01@gmail.com>
2026-08-19 12:44:50 +0100

    Update parametres.tsx

--- src/routes/_authenticated/parametres.tsx
+++ src/routes/_authenticated/parametres.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState, useMemo } from "react";
 import { toast } from "sonner";
 import { supabase } from "@/integrations/supabase/client";
 import { Button } from "@/components/ui/button";
-import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
