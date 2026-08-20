2d8d9e643fc1349903c2702b6b61692446ea99e3
ahmatsamah01-cell <ahmatsamah01@gmail.com>
2026-08-20 21:26:21 +0100

    Update use-hotel.ts

--- src/hooks/use-hotel.ts
+++ src/hooks/use-hotel.ts
@@ -50,4 +50,4 @@ export function useMonRole() {
         estReception: roles.includes("reception"),
         estComptable: roles.includes("comptable"),
         email: userData.user?.email ?? null 
-      };
+      }
